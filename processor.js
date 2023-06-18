f0dcdbd7d47cb470f75176530eabb050682b6dd4
cengels <cengels@tuta.io>
2023-06-18 17:27:56 +0200

    Remove duplicate events

--- processor.js
+++ processor.js
@@ -1,10 +1,20 @@
 export function processTimeline(timeline) {
     const actions = {};
 
+    let precedingTimestamp = 0;
+
     return {
         actions,
         timeline: timeline.events
-            .filter(event => !event.ability.name.startsWith('unknown'))
+            .filter(event => {
+                if (event.timestamp === precedingTimestamp) {
+                    return false;
+                }
+
+                precedingTimestamp = event.timestamp;
+
+                return !event.ability.name.startsWith('unknown');
+            })
             .map(event => {
                 if (actions[event.ability.guid] == null) {
                     actions[event.ability.guid] = { description: 'Placeholder description for ' + event.ability.name };
