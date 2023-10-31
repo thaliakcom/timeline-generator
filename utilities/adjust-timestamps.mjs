152969b0059558e7f65fc71ec1ea1548f3863c55
cengels <cengels@tuta.io>
2023-11-01 00:22:24 +0100

    Add adjust-timestamps script

---
+++ utilities/adjust-timestamps.mjs
@@ -0,0 +1,21 @@
+import yaml from 'js-yaml';
+import clipboardy from 'clipboardy';
+
+const text = process.argv[2] === 'clipboard' ? clipboardy.readSync() : process.argv[2];
+const adjustBy = Number(process.argv[3]);
+
+if (Number.isNaN(adjustBy)) {
+    throw new Error(`Invalid number '${process.argv[3]}'.`);
+}
+
+const json = yaml.load(text);
+
+for (const item of json) {
+    item.at += adjustBy;
+}
+
+const result = yaml.dump(json);
+
+clipboardy.writeSync(result);
+
+console.log(`Result copied to clipboard!`);
